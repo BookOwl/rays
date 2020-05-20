e53e9b610987d3bf53543d17f8d4003c8bd0ddb6
BookOwl <stanleybookowl@gmail.com>
2020-05-20 11:41:44 -0400

    Add dependency on the image crate and a demo using that crate
    While the book creates files in the PPM format, I didn't want to have to find a new program to open the files in.
    Adding the image crate lets me make PNG files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use image::{RgbImage, Rgb};
+
 fn main() {
-    println!("Hello, world!");
+    let width = 256;
+    let height = 256;
+    let mut img = RgbImage::new(width, height);
+    for (y, j) in (0..height).rev().enumerate() {
+        for (x, i) in (0..width).enumerate() {
+            let r = (i as f32) / (width as f32 - 1.0);
+            let g = (j as f32) / (height as f32 - 1.0);
+            let b = 0.25;
+            let pixel = Rgb([(255.999 * r) as u8, (255.999 * g) as u8, (255.999 * b) as u8]);
+            img.put_pixel(x as u32, y as u32, pixel);
+        }
+    }
+    img.save("out.png").unwrap();
 }
